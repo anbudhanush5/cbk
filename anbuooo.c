6b17a2bb84ad75185ba67e2117f3a196c5508668
anbudhanush5 <35687834+anbudhanush5@users.noreply.github.com>
2018-02-07 14:26:42 +0530

    Create anbuooo.c

---
+++ anbuooo.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    printf("Print Odd Numbers in a given range m to n:\n");
+
+    for (num = m; num <= n; num++)
+
+        {
+
+               if (num % 2 == 1)
+
+                  printf ("%d ", num);
+
+         }
+
+                getch();
+
+  }
